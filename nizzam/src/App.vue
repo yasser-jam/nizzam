ad0e74b2295c0e125475e766ab6054a9f98af3db
yasser-jamal-aldeen <yasserjamalaldeen@gmail.com>
2022-03-29 23:57:46 +0300

    remove unnecessary files

---
+++ nizzam/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <default-layout>
+
+  </default-layout>
+</template>
+<script>
+import defaultLayout from './layouts/DefaultLayout.vue'
+
+export default {
+  name: 'App',
+  components: {
+    defaultLayout
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
