7ac6cf5ce9e9ef3ad30409a184a192588cd4d5b8
yasser-jamal-aldeen <yasserjamalaldeen@gmail.com>
2022-03-30 00:00:34 +0300

    add vuetify

--- nizzam/src/App.vue
+++ nizzam/src/App.vue
@@ -1,26 +1,23 @@
 <template>
-  <default-layout>
-
-  </default-layout>
+  <v-app>
+    <v-main>
+      <HelloWorld/>
+    </v-main>
+  </v-app>
 </template>
+
 <script>
-import defaultLayout from './layouts/DefaultLayout.vue'
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
+
   components: {
-    defaultLayout
-  }
+    HelloWorld,
+  },
+
+  data: () => ({
+    //
+  }),
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
