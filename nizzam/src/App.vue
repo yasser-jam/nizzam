d7da3de73cfb530e6e15d047019e7698a99bc9f6
yasser-jamal-aldeen <yasserjamalaldeen@gmail.com>
2022-04-14 01:15:25 +0300

    improve appearance

--- nizzam/src/App.vue
+++ nizzam/src/App.vue
@@ -1,9 +1,6 @@
 <template>
-  <v-app id="inspire">
-    <v-navigation-drawer
-      v-model="drawer"
-      app
-    >
+  <v-app>
+    <v-navigation-drawer v-model="drawer" app>
       <!--  -->
     </v-navigation-drawer>
 
@@ -14,13 +11,38 @@
     </v-app-bar>
 
     <v-main>
-      <!--  -->
+      <v-container>
+        <v-row>
+          <v-col cols="12" lg="4">
+            <todo-list></todo-list>
+          </v-col>
+          <v-col cols="12" lg="8">
+            <year-goals></year-goals>
+          </v-col>
+        </v-row>
+      </v-container>
     </v-main>
   </v-app>
 </template>
 
 <script>
-  export default {
-    data: () => ({ drawer: null }),
+import TodoList from './components/TodoList.vue'
+import YearGoals from './components/YearGoals.vue'
+export default {
+  data: () => ({
+    drawer: null,
+    
+  }),
+  components: {
+    TodoList,
+    YearGoals
   }
+};
 </script>
+
+<style>
+label {
+  color: #111;
+  opacity: 1;
+}
+</style>
